2822942cb75986ebd1b556facdae325d66630625
Victor Cunha <vrcbr17@gmail.com>
2021-05-01 01:02:40 -0300

    add kelvin func

--- index.js
+++ index.js
@@ -21,6 +21,34 @@ app.get('/celsius/:valor/fahrenheit', (req, res) => {
     res.json({ "fahrenheit": fahrenheit, "maquina": os.hostname() });
 });
 
+app.get('/kelvin/:valor/celsius', (req, res) => {
+
+    let valor = req.params.valor;
+    let celsius = valor - 273.15;
+    res.json({ "celsius": celsius, "maquina": os.hostname() });
+});
+
+app.get('/celsius/:valor/kelvin', (req, res) => {
+
+    let valor = req.params.valor;
+    let kelvin = valor + 273.15;
+    res.json({ "kelvin": kelvin, "maquina": os.hostname() });
+});
+
+app.get('/temperatura/kelvinparacelsius/:valor', (req, res) => {
+
+    let valor = req.params.valor;
+    let celsius = valor - 273.15;
+    res.json({ "celsius": celsius });
+});
+
+app.get('/temperatura/celsiusparakelvin/:valor', (req, res) => {
+
+    let valor = req.params.valor;
+    let kelvin = valor + 273.15;
+    res.json({ "kelvin": kelvin });
+});
+
 app.get('/temperatura/fahrenheitparacelsius/:valor', (req, res) => {
 
     let valor = req.params.valor;
